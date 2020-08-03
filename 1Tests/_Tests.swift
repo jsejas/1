b6cea0ec19ca7db4f55e912a3eed6e5563c65099
Joel Sejas <69014144+jsejas@users.noreply.github.com>
2020-08-02 23:50:48 -0700

    Initial Commit

---
+++ 1Tests/_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _Tests.swift
+//  1Tests
+//
+//  Created by Joel Sejas on 8/2/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import XCTest
+@testable import _
+
+class _Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
